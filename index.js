47c43fc4453d4f2273d2a5daca4dec96a664402d
Yuval Fishbine <YuvalFish12@gmail.com>
2024-10-20 16:50:21 +0300

    add home page ejs functionality

--- index.js
+++ index.js
@@ -7,8 +7,35 @@ const PORT = 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let books = [
+    {id: 1, name: 'Think And Grow Rich', author: 'Napoleon Hill', category_id: 2, cover_image: 'assets/think-and-grow-rich.jpg'},
+    {id: 2, name: 'Think And Grow Rich', author: 'Napoleon Hill', category_id: 2, cover_image: 'assets/think-and-grow-rich.jpg'},
+];
+
+let categories = [
+    {id: 1, name: 'All'},
+    {id: 2, name: 'Finance'},
+    {id: 3, name: 'Personal Development'},
+    {id: 4, name: 'Business'}
+];
+
+let current_user = 1;
+
+let users = [
+    {id: 1, name: 'Yuval'},
+    {id: 2, name: 'Someone'},
+    {id: 3, name: 'Another Guy'}
+];
+
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", 
+        {
+            books: books,
+            categories: categories,
+            current_user: users.find((user) => user.id === current_user),
+            users: users
+        }
+    );
 });
 
 app.listen(PORT, () => {
